9c437b475e0b082d53f152c252fa7f3c7752b670
ali-sshff <sharipoff.dev@gmail.com>
2023-09-18 18:46:53 +0200

    small changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import SharedProducts from '../components/SharedProducts'
 import Categories from '../components/Categories'
+import About from '../components/About'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <SharedProducts />
       <Categories />
+      <About />
     </div>
   )
 }
